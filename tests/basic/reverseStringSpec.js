a039a06e727e800d7bf616a0995b062cc4158ee1
cleriphil <cleri.phil@gmail.com>
2016-06-15 20:23:34 -0700

    add and pass all specs for reverseString

--- tests/basic/reverseStringSpec.js
+++ tests/basic/reverseStringSpec.js
@@ -1,5 +1,11 @@
 describe("reverseString", function() {
-  it('should reverse "hello"', function() {
+  it('should reverse a word in all lowercase', function() {
     expect(reverseString('hello')).toBe('olleh');
   });
+  it('should reverse a word with a capital and lowercase letters', function() {
+    expect(reverseString('youTube')).toBe('ebuTuoy');
+  });
+  it('should reverse a phrase with spaces', function() {
+    expect(reverseString('Welcome to Portland')).toBe('dnaltroP ot emocleW');
+  })
 });
