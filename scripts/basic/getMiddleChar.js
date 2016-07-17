1c7a8f4dfb3c23cd50f861315b2005e106461516
cleriphil <cleri.phil@gmail.com>
2016-07-17 12:11:11 -0700

    run and pass specs for getMiddleChar

--- scripts/basic/getMiddleChar.js
+++ scripts/basic/getMiddleChar.js
@@ -1,7 +1,17 @@
 // You are going to be given a word.
 // Your job is to return the middle character of the word.
-// If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+// If the word's length is odd, return the middle character.
+//If the word's length is even, return the middle 2 characters.
 
 function getMiddleChar(str) {
-
+ var strLength = str.length;
+ var arr = str.split("");
+ var middle = "";
+ var halfIndex = strLength/2;
+ if(strLength % 2 === 0) {
+   middle += (arr[halfIndex - 1]) += (arr[halfIndex]);
+ } else {
+   middle += arr[Math.floor(halfIndex)];
+ }
+ return middle;
 }
--- tests/basic/getMiddleCharSpec.js
+++ tests/basic/getMiddleCharSpec.js
@@ -1,14 +1,14 @@
 describe("getMiddleChar", function(){
   it('should return "es" when given "test"', function(){
-    expect(getMiddleChar("test").toEqual("es"));
+    expect(getMiddleChar("test")).toEqual("es");
   });
   it('should return "t" when given "testing"', function(){
-    expect(getMiddleChar("testing").toEqual("t"));
+    expect(getMiddleChar("testing")).toEqual("t");
   });
   it('should return "dd" when given "middle"', function(){
-    expect(getMiddleChar("middle").toEqual("dd"));
+    expect(getMiddleChar("middle")).toEqual("dd");
   });
   it('should return "A" when given "A"', function(){
-    expect(getMiddleChar("A").toEqual("A"));
+    expect(getMiddleChar("A")).toEqual("A");
   });
 });
