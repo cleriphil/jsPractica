f274e1423649ae85e1d0a20f5fc704263abfbd55
cleriphil <cleri.phil@gmail.com>
2016-07-17 11:41:58 -0700

    run and pass specs for findLongestWord (map and sort methods)

--- scripts/basic/findLongestWord.js
+++ scripts/basic/findLongestWord.js
@@ -1,3 +1,12 @@
+// Return the length of the longest word in the provided sentence.
+
 function findLongestWord(str) {
-  return 0;
+  var arr = str.split(' ');
+  var lengths = arr.map(function(val){
+      return val.length;
+  });
+  var sortLengths = lengths.sort(function(a, b){
+       return b - a;
+  });
+  return sortLengths[0];
 }
